dbd71bfb6c937acfc9b00c8426a20f05bebd6dc5
Pablo Sánchez <pablosanchezflores@gmail.com>
2014-10-17 13:43:39 +0200

    Corregido números, elección y tirada

--- cuatroraya.py
+++ cuatroraya.py
@@ -1,26 +1,33 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 import sys
+import string
+tablero = [["_","_","_","_","_","_","_","_","_"],["_","_","_","_","_","_","_","_","_"],["_","_","_","_","_","_","_","_","_"],["_","_","_","_","_","_","_","_","_"],["_","_","_","_","_","_","_","_","_"],["_","_","_","_","_","_","_","_","_"], ["1","2","3","4","5","6","7","8","9"]]
+tirada = 'X'
 
-tablero = [["_","_","_","_","_","_","_","_","_"],["_","_","_","_","_","_","_","_","_"],["_","_","_","_","_","_","_","_","_"],["_","_","_","_","_","_","_","_","_"],["_","_","_","_","_","_","_","_","_"],["_","_","_","_","_","_","_","_","_"]]
 def mostrar(tablero):
-	tc = range(6)
+	tc = range(7)
 	tf = range(9)
 	for i in tc:
 		for j in tf:
 			sys.stdout.write('|')
-			sys.stdout.write(tablero[i][j])
+			sys.stdout.write(tablero[i][j])		
 		sys.stdout.write('|')
 		print("")
+def elige():
+	j = ' '
+	while j<> 'O' and j<> 'X':
+		j =raw_input("Elige una ficha(O/X): ")
+	if j == 'O':
+		maquina='X'
+		jugador='O'
+	else:
+		jugador='X'
+		maquina='O'
+
+	print("Has elegido "+ jugador)
+def tirada():
+	if jugador==tirada:
+		t = raw_input("Tira: ")
 mostrar(tablero)
-j = ' '
-while j<> '#' and j<> '@':
-	print("Elige una ficha: ");
-	j= input()
-if j== '#':
-	maquina='#'	
-	jugador='@'
-	turno='h'
-else:
-	jugador='#'
-	maquina='@'
-print("Has elegido "+ jugador)
+elige()
+tirada()
