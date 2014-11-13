da9af8fe8b8b6131c7fcb035986cf41edf6386f7
Pablo Sánchez <pablosanchezflores@gmail.com>
2014-11-13 12:17:38 +0100

    subo cambios

---
+++ practica_2/tabla.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tabla (
+id serial primary key,
+usuario varchar(45) not null,
+contrasena varchar(45) not null,
+correo varchar(45) not null,
+nombre varchar(45) not null,
+apellido varchar(45) not null,
+descripcion varchar(150) not null
+)
