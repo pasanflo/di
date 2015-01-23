e7efd947826144960d6f550449511e4dc531dd96
Pablo Sánchez <pablosanchezflores@gmail.com>
2015-01-23 12:27:21 +0100

    Subo ejercicio de mantenimiento acabado.

--- mantenimiento_pablosanchez/modificar.php
+++ mantenimiento_pablosanchez/modificar.php
@@ -3,10 +3,10 @@
 	include "configuracion.php";
 	cabecera("Modificar registro");
 ?>
-<form method="get" name="formulario" action="modifica.php">
-	Inserta ID para modificar <input type="text" value="" name="id" size="3"></br>
-	<input type="submit" value="Modificar">
-</form>
+	<form method="get" name="formulario" action="edicion.php">
+		Inserta ID para modificar <input type="text" value="" name="id" size="3"></br>
+		<input type="submit" value="Modificar">
+	</form>
 <?php
 	pie();
 ?>
